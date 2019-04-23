214e9b4dc4c672a4571d07b63bfe4035c37640f4
Niels Bunkenburg <stu114713@informatik.uni-kiel.de>
2019-04-23 12:39:12 +0200

    Adapt logo and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scp -i ~/.ssh/server_rsa -r /home/nbu/Documents/website/theProfessional-hakyll/_site/* niels@bunkenburg.net:/var/www/html
+rsync -avz --delete /home/nbu/Documents/website/theProfessional-hakyll/_site/ niels@bunkenburg.net:/var/www/html
